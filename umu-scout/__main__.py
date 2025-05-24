5f5b628481066744f30a08683ef12bf64572f354
Stelios Tsampas <loathingkernel@gmail.com>
2025-05-24 18:59:39 +0300

    umu-scout: fetch latest json from github before packaging

--- umu-scout/__main__.py
+++ umu-scout/__main__.py
@@ -1,4 +1,5 @@
 import json
+import os
 import shutil
 import sys
 import tarfile
@@ -16,19 +17,45 @@ steam_runtime_ver_url = "https://repo.steampowered.com/steamrt1/images/latest-pu
 steam_runtime_csum_url = "https://repo.steampowered.com/steamrt1/images/latest-public-beta/steam-runtime.tar.xz.checksum"
 
 package_parts =  ("app1070560", "steam-runtime")
-
 package_name = "umu-scout"
+package_version_file = f"{package_name}.version.json"
+
+github_repo = os.environ.get("UMU_SCOUT_REPO", f"Open-Wine-Components/{package_name}")
+github_headers: dict[str, str] = {
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28",
+    "User-Agent": "",
+}
+github_latest_url = f"https://api.github.com/repos/{github_repo}/releases/latest"
+
+
+def get_latest_release() -> dict | None:
+    latest: dict | None = None
+
+    with urlopen(github_latest_url) as url_fd:
+        release = json.load(url_fd)
+
+    if release is not None:
+        asset = next((a for a in release["assets"] if a["name"] == package_version_file), None)
+        if asset is not None:
+            with urlopen(asset["browser_download_url"]) as url_fd:
+                latest = json.load(url_fd)
+        else:
+            print(f"Could not find asset {package_version_file} at https://github.com/{github_repo}", file=sys.stderr)
+    else:
+        print(f"Could not find latest release at https://github.com/{github_repo}", file=sys.stderr)
+
+    return latest
 
 
 if __name__ == "__main__":
     old_ver: dict | None = None
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1 and sys.argv[1] == "update":
         try:
-            with open(sys.argv[1], "r") as fd:
-                old_ver = json.loads(fd.read())
+            old_ver = get_latest_release()
         except Exception as e:
-            print(f"Could not read old versions from {sys.argv[1]}: {str(e)}", file=sys.stderr)
-            sys.exit(1)
+            print(f"Could not read old versions from {github_latest_url}: {str(e)}", file=sys.stderr)
+            print("Downloading latest", file=sys.stderr)
     else:
         print("Downloading latest", file=sys.stderr)
 
@@ -36,9 +63,11 @@ if __name__ == "__main__":
         "app1070560": urlopen(app1070560_ver_url).read().strip().decode("utf-8"),
         "steam-runtime": urlopen(steam_runtime_ver_url).read().strip().decode("utf-8"),
     }
+    versions["tag"] = '+'.join([versions[part] for part in package_parts])
 
     for part in package_parts:
         print(f"{part}: {old_ver.get(part) if old_ver else None} -> {versions[part]}", file=sys.stderr)
+    print(f"tag: {old_ver.get('tag') if old_ver else None} -> {versions['tag']}", file=sys.stderr)
 
     if old_ver is not None and all([old_ver.get(p) == versions[p] for p in package_parts]):
         print("Already up to date", file=sys.stderr)
@@ -48,14 +77,14 @@ if __name__ == "__main__":
         shutil.rmtree(dist)
     dist.mkdir(exist_ok=True)
 
-    with open(dist.joinpath(f"{package_name}.version.json"), "w") as versions_json:
+    with open(dist.joinpath(package_version_file), "w") as versions_json:
         versions_json.write(json.dumps(versions))
 
     with tarfile.open(name=None, fileobj=BytesIO(urlopen(app1070560_tar_url).read())) as tar:
-        tar.extractall(dist)
+        tar.extractall(dist, filter="fully_trusted")
 
     with tarfile.open(name=None, fileobj=BytesIO(urlopen(steam_runtime_tar_url).read())) as tar:
-        tar.extractall(dist.joinpath("SteamLinuxRuntime"))
+        tar.extractall(dist.joinpath("SteamLinuxRuntime"), filter="fully_trusted")
 
     urlretrieve(steam_runtime_csum_url, dist.joinpath("SteamLinuxRuntime", "steam-runtime.tar.xz.checksum"))
 
@@ -65,5 +94,8 @@ if __name__ == "__main__":
         format="xztar",
         root_dir=dist,
     )
+
+    # to stdout to be captured as tag
+    print(versions["tag"], file=sys.stdout)
 
     sys.exit(0)
